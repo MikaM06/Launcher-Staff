af715c98ed1c9d78f53762b913b9627a3f82e73c
Link34500 <160628001+Link34500@users.noreply.github.com>
2024-06-09 11:55:21 -0300

    publish 2.1.1

--- src/assets/js/launcher.js
+++ src/assets/js/launcher.js
@@ -31,12 +31,6 @@ class Launcher {
     }
 
     initLog() {
-        document.addEventListener('keydown', e => {
-            if (e.ctrlKey && e.shiftKey && e.keyCode == 73 || e.keyCode == 123) {
-                ipcRenderer.send('main-window-dev-tools-close');
-                ipcRenderer.send('main-window-dev-tools');
-            }
-        })
         new logger(pkg.name, '#7289da')
     }
 
